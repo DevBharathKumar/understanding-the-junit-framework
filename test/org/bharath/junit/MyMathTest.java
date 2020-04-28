c1ff9a086c4672e4b7c970dca0f42b8c40b27929
Bharath Kumar <bharathk494@gmail.com>
2020-04-28 19:12:07 +0530

    Understanding @BeforeEach, @AfterEach, @BeforeAll and @AfterAll of J Unit 5

--- test/org/bharath/junit/MyMathTest.java
+++ test/org/bharath/junit/MyMathTest.java
@@ -1,23 +1,56 @@
 package org.bharath.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
 	MyMath myMath = new MyMath();
 	
+	int x;
+	
+	@BeforeEach
+	void before() {
+		System.out.println("Execute before each method ");
+	}
+	
 	@Test
 	void test3Numbers() 
 	{
+		System.out.println("Hello 3");
 		assertEquals(6, myMath.sumNumbers(new int[] {1,2,3}));
 	}
 	
 	@Test
 	void test1Numbers() 
 	{
+		System.out.println("Hello 1");
 		assertEquals(9, myMath.sumNumbers(new int[] {9}));
+	}
+	
+	
+	
+	@AfterEach
+	void executeAfterEachMethod() {
+		System.out.println("Execute after each method ");
+	}
+	
+	@BeforeAll
+	static void executeBeforeClassMethod() {
+		System.out.println("Executed at the beginning of the class ");
+	}
+	
+	@AfterAll
+	static void executeAfterClassMethod() {
+		System.out.println("Executed at the end of the class ");
 	}
 
 }
